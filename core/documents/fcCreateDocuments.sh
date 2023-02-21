3024e104aafd0e10b66669e1b8b59ff026bca535
DJeovannyZ <darwinzaldana@gmail.com>
2023-02-21 16:13:13 -0600

    Script modified

--- core/documents/fcCreateDocuments.sh
+++ core/documents/fcCreateDocuments.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 function createFiles {
-    touch "$dirSelected/prueba.txt"
 }
 
--- core/utils/fcPaths.sh
+++ core/utils/fcPaths.sh
@@ -3,6 +3,7 @@
 
 # Function to set default path
 function setDefaultPath {
+  
   # Check if all parameters are provided
   if [ "$#" -ne 1 ]; then
     echo "Error: to setDefaultPath expecify the path of workspace. use uniUtils --help for more information."
@@ -37,6 +38,23 @@ function checkDefaultPath {
   # Check if default path file exists
   if [ ! -f "$HOME/.config/uniUtils/default_path.txt" ]; then
     touch $HOME/.config/uniUtils/default_path.txt
+  fi
+
+  # Obtenemos la ruta del script "fcPaths.sh"
+  script_path="$(realpath "${BASH_SOURCE[0]}")"
+  script_dir="$(dirname "$script_path")"
+  cd "$script_dir"
+  cd ../../resources/
+  
+  # Chequeamos si existen los archivos y los copiamos si es necesario
+  if [ ! -f "$HOME/.config/uniUtils/DocWord.docx" ]; then
+      echo "Copiando archivo DocWord.docx"
+      cp "DocWord.docx" "$HOME/.config/uniUtils/"
+  fi
+  
+  if [ ! -f "$HOME/.config/uniUtils/DocExcel.xlsx" ]; then
+      echo "Copiando archivo DocExcel.xlsx"
+      cp "DocExcel.xlsx" "$HOME/.config/uniUtils/"
   fi
 
   # Check if default path file has a path set
