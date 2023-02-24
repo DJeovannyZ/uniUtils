df9287a5c5c013ec1f0c4f46a332bff31d23394d
DJeovannyZ <darwinzaldana@gmail.com>
2023-02-24 10:39:49 -0600

    Agregando rclone sync

--- core/documents/fcSynchroniceDocs.sh
+++ core/documents/fcSynchroniceDocs.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 #
 function pushFiles {
-  selectFiles "$defaultPath"
-  for file in "${selectedFiles[@]}"
-  do
-      fileName=$(basename "$file")
-      fileDrive=$(echo "$file" | sed "s|$HOME/||")
-      dirPathDrive=$(dirname "$fileDrive")
-      echo "Borrando version antigua de $fileName de google drive"
-      rclone delete gdrive:"$fileDrive" -P
-      echo "Subiendo version actualizada de $fileName a google drive"
-      rclone copy "$file" gdrive:"$dirPathDrive" -P
-  done
-
+  dirDrive=$(echo "$defaultPath" | sed "s|$HOME/||")
+  echo "Subiendo archivos de $defaultPath a Google Drive"
+  rclone sync "$defaultPath" gdrive:"$dirDrive" -P
 }
 
 function pullFiles {
-  selectFiles "$defaultPath"
-  for file in "${selectedFiles[@]}"
-  do
-      fileName=$(basename "$file")
-      fileDrive=$(echo "$file" | sed "s|$HOME/||")
-      dirFile=$(dirname "$file")
-      rm "$file"
-      echo "Borrando version antigua de $fileName de local"
-      echo "Descargando version actualizada de $fileName de google drive"
-      rclone copy gdrive:"$fileDrive" "$dirFile" -P
-  done
+  dirDrive=$(echo "$defaultPath" | sed "s|$HOME/||")
+  echo "Descargando archivos nuevos de Google Drive a $defaultPath"
+  rclone sync gdrive:"$dirDrive" "$defaultPath" -P
 }
 
 function deleteFiles {
@@ -40,5 +23,4 @@ function deleteFiles {
       rm "$file"
       echo "Borrando $fileName de local"
   done
-
 }
